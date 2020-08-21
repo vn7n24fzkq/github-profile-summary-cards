f060b60e4b9669695a8ee79026f69db94e047c42
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-08-21 16:40:20 +0800

    Add icon color

--- src/const/theme.js
+++ src/const/theme.js
@@ -4,12 +4,14 @@ const Theme = {
         text_color:"#000000",
         bg_color:"#ffffff",
         stroke_color:"#657b83",
+        icon_color:"#657b83",
     },
    solarized: {
        title_color:"#586e75",
         text_color:"#586e75",
         bg_color:"#fdf6e3",
         stroke_color:"#fdf6e3",
+        icon_color:"#657b83",
     },
 };
 
