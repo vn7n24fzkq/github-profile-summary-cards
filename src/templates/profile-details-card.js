18deb0234e3eb7a4a40d77897bd0d8cfb29bb48d
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-08-22 23:51:27 +0800

    Update comment

--- src/templates/profile-details-card.js
+++ src/templates/profile-details-card.js
@@ -2,20 +2,13 @@ const getRepoLanguage = require("../github-api/profile-details");
 const Card = require("./card");
 const d3 = require("d3");
 
-
 function createDetailCard(title, userDetails, theme) {
-
   let card = new Card(title, 800, 200, theme);
   let svg = card.getSVG();
 
-  //draw language node
+  //draw icon
 
-  const panel = svg
-    .append("g")
-    .attr(
-      "transform",
-      `translate(30,30)`
-    );
+  const panel = svg.append("g").attr("transform", `translate(30,30)`);
   let labelHeight = 14;
   panel
     .selectAll(null)
@@ -23,16 +16,15 @@ function createDetailCard(title, userDetails, theme) {
     .enter()
     .append("g")
     .attr("transform", (d) => {
-        let y = labelHeight * d.index * 2;
-        return `translate(0,${y})`;
-    }  )
+      let y = labelHeight * d.index * 2;
+      return `translate(0,${y})`;
+    })
     .attr("width", labelHeight)
     .attr("height", labelHeight)
     .attr("fill", theme.text_color)
-    .html((d)=>d.icon);
+    .html((d) => d.icon);
 
-
-  //set langu text
+  //draw text
   panel
     .selectAll(null)
     .data(userDetails)
@@ -42,7 +34,7 @@ function createDetailCard(title, userDetails, theme) {
       return d.value;
     })
     .attr("x", labelHeight * 1.5)
-    .attr("y", (d) => labelHeight * d.index * 2  +labelHeight)
+    .attr("y", (d) => labelHeight * d.index * 2 + labelHeight)
     .style("fill", theme.text_color)
     .style("font-family", "'Lucida Sans Unicode', 'Lucida Grande', sans-serif")
     .style("font-size", `${labelHeight}px`);
