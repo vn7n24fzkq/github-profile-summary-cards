926af401425f6fe8f4a2890a4700fd6c0e18bcae
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-08-24 00:34:33 +0800

    Add contributions chart

--- src/templates/profile-details-card.js
+++ src/templates/profile-details-card.js
@@ -1,9 +1,10 @@
 const getRepoLanguage = require("../github-api/profile-details");
 const Card = require("./card");
 const d3 = require("d3");
+const moment = require("moment");
 
-function createDetailCard(title, userDetails, theme) {
-  let card = new Card(title, 800, 200, theme);
+function createDetailCard(title, userDetails, contributionsData, theme) {
+  let card = new Card(title, 700, 200, theme);
   let svg = card.getSVG();
 
   //draw icon
@@ -38,6 +39,90 @@ function createDetailCard(title, userDetails, theme) {
     .style("fill", theme.text_color)
     .style("font-family", "'Lucida Sans Unicode', 'Lucida Grande', sans-serif")
     .style("font-size", `${labelHeight}px`);
+
+  //process chart data
+  let lineChartData = [];
+  for (let data of contributionsData) {
+    let formatDate = moment(data.date).format("YYYY-MM");
+    data.date = new Date(formatDate);
+    let lastIndex = lineChartData.length - 1;
+    if (
+      lineChartData.length == 0 ||
+      lineChartData[lastIndex].date.getTime() !== data.date.getTime()
+    ) {
+      lineChartData.push({
+        contributionCount: data.contributionCount,
+        date: data.date,
+      }); //use new object
+    } else {
+      lineChartData[lastIndex].contributionCount += data.contributionCount;
+    }
+  }
+
+  //prepare chart data
+  let chartRightMargin = 30;
+  let chartWidth = card.width - 2 * card.xPadding - chartRightMargin - 230;
+  let chartHeight = card.height - 2 * card.yPadding;
+  var x = d3.scaleTime().range([0, chartWidth]);
+  x.domain(
+    d3.extent(lineChartData, function (d) {
+      return d.date;
+    })
+  );
+
+  let yMax = Math.max.apply(
+    Math,
+    lineChartData.map(function (o) {
+      return o.contributionCount;
+    })
+  );
+
+  let y = d3.scaleLinear().range([chartHeight, 0]);
+  y.domain([0, yMax]);
+
+  let valueline = d3
+    .area()
+    .x(function (d) {
+      return x(d.date);
+    })
+    .y0(y(0))
+    .y1(function (d) {
+      return y(d.contributionCount);
+    })
+    .curve(d3.curveMonotoneX);
+
+  let chartPanel = svg
+    .append("g")
+    .attr(
+      "transform",
+      `translate(${card.width - chartWidth - card.xPadding},0)`
+    );
+
+  //draw chart line
+  chartPanel
+    .append("path")
+    .data([lineChartData])
+    .attr("transform", `translate(${-chartRightMargin},0)`)
+    .attr("fill", theme.icon_color)
+    .attr("stroke", "none")
+    .attr("d", valueline);
+
+  //Add the X Axis
+  var xAxis_woy = d3
+    .axisBottom(x)
+    .tickFormat(d3.timeFormat("%y/%m"))
+    .tickValues(lineChartData.map((d) => d.date));
+
+  chartPanel
+    .append("g")
+    .attr("transform", `translate(${-chartRightMargin},${chartHeight})`)
+    .call(xAxis_woy);
+
+  //Add the Y Axis
+  chartPanel
+    .append("g")
+    .attr("transform", `translate(${chartWidth - chartRightMargin},0)`)
+    .call(d3.axisRight(y));
 
   return card.toString();
 }
