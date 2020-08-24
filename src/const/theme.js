9e1e97b10d8c82aac539e197e9b3e6fdf1c6c509
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-08-24 14:13:37 +0800

    Update theme

--- src/const/theme.js
+++ src/const/theme.js
@@ -1,17 +1,19 @@
 const Themes = {
     default: {
-        title_color:"#000000",
-        text_color:"#000000",
+        title_color:"#586e75",
+        text_color:"#586e75",
         bg_color:"#ffffff",
-        stroke_color:"#657b83",
-        icon_color:"#657b83",
+        stroke_color:"#586e75",
+        icon_color:"#586e75",
+        line_chart_color:"#586e75",
     },
    solarized: {
-       title_color:"#586e75",
+        title_color:"#586e75",
         text_color:"#586e75",
         bg_color:"#fdf6e3",
         stroke_color:"#fdf6e3",
-        icon_color:"#657b83",
+        icon_color:"#586e75",
+        line_chart_color:"#268bd2",
     },
 };
 
