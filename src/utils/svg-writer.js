545c4339c804e0c8b51005ce753226b7fd0458a2
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-08-29 18:07:51 +0800

    Add file write test

--- src/utils/svg-writer.js
+++ src/utils/svg-writer.js
@@ -2,14 +2,12 @@ const fs = require("fs");
 const outputPath = "./profile-summary-card-output/";
 const writeSVG = function (folder,filename, svgString) {
   const targetFolder = `${outputPath}${folder}/`
-  fs.mkdir(targetFolder, { recursive: true }, (err) => {
-    if (err) throw err;
-    fs.writeFileSync(`${targetFolder}${filename}.svg`, svgString, function (
-      err,
-      result
-    ) {
-      if (err) throw (err);
-    });
+  fs.mkdirSync(targetFolder, { recursive: true });
+  fs.writeFileSync(`${targetFolder}${filename}.svg`, svgString, function (
+    err,
+    result
+  ) {
+    if (err) throw (err);
   });
 };
 
--- tests/utils/svg-writer.test.js
+++ tests/utils/svg-writer.test.js
@@ -1,6 +1,7 @@
 const { writeSVG, outputPath } = require("../../src/utils/svg-writer");
 const fs = require("fs");
 
+
 describe("Test output function", () => {
   it("test write svg can work", () => {
     writeSVG("test", "write-svg", "work");
