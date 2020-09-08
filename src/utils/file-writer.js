2c55a46c57ede82f8a871673180c949f82e92343
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-09-08 23:44:29 +0800

    Add token details in generate preview

--- src/utils/file-writer.js
+++ src/utils/file-writer.js
@@ -38,6 +38,10 @@ const generatePreviewMarkdown = function (isInGithubAction) {
 # Preview Cards
 
 Here are all cards with themes.
+| :warning: | If your workflow does not generate all cards in output folder, then you need to use [Personal access token](https://docs.github.com/en/actions/configuring-and-managing-workflows/creating-and-storing-encrypted-secrets) instead of GITHUB_TOKEN in workflow. |
+| :-------: | :------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ |
+
+[Personal token need those permission](https://github.com/vn7n24fzkq/github-profile-summary-cards/wiki/Personal-access-token-permissions)
 
 `;
 
