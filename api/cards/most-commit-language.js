d3bd9b37c2508c30bf8bd94b726c6c39d7c9f22d
Casper <vn7n24fzkq@gmail.com>
2021-04-08 19:33:23 +0800

    feat: GitHub api retry (#42)
    * Add github token updater & error card
    
    * Fix missing required module
    
    * Fix constant variable
    
    * Fix github token updater
    
    * Update github updater
    
    * Update github token index
    
    * Add github token updater log
    
    * Update token name
    
    * Update web host
    
    * Add github button
    
    * Update web UI

--- api/cards/most-commit-language.js
+++ api/cards/most-commit-language.js
@@ -1,10 +1,31 @@
 const {
     getCommitsLanguageSVGWithThemeName,
 } = require('../../src/cards/most-commit-lauguage-card');
+const { changToNextGitHubToken } = require('../utils/github-token-updater');
+const { getErrorMsgCard } = require('../utils/error-card');
 
 module.exports = async (req, res) => {
     const { username, theme } = req.query;
-    const cardSVG = await getCommitsLanguageSVGWithThemeName(username, theme);
-    res.setHeader('Content-Type', 'image/svg+xml');
-    res.send(cardSVG);
+    try {
+        let tokenIndex = 0;
+        while (true) {
+            try {
+                const cardSVG = await getCommitsLanguageSVGWithThemeName(
+                    username,
+                    theme
+                );
+                res.setHeader('Content-Type', 'image/svg+xml');
+                res.send(cardSVG);
+                return;
+            } catch (err) {
+                console.log(err.message);
+                // We update github token and try again, until getNextGitHubToken throw an Error
+                changToNextGitHubToken(tokenIndex);
+                tokenIndex += 1;
+            }
+        }
+    } catch (err) {
+        console.log(err);
+        res.send(getErrorMsgCard(err.message, theme));
+    }
 };
--- api/cards/stats.js
+++ api/cards/stats.js
@@ -1,8 +1,26 @@
 const { getStatsSVGWithThemeName } = require('../../src/cards/stats-card');
+const { changToNextGitHubToken } = require('../utils/github-token-updater');
+const { getErrorMsgCard } = require('../utils/error-card');
 
 module.exports = async (req, res) => {
     const { username, theme } = req.query;
-    const cardSVG = await getStatsSVGWithThemeName(username, theme);
-    res.setHeader('Content-Type', 'image/svg+xml');
-    res.send(cardSVG);
+    try {
+        let tokenIndex = 0;
+        while (true) {
+            try {
+                const cardSVG = await getStatsSVGWithThemeName(username, theme);
+                res.setHeader('Content-Type', 'image/svg+xml');
+                res.send(cardSVG);
+                return;
+            } catch (err) {
+                console.log(err.message);
+                // We update github token and try again, until getNextGitHubToken throw an Error
+                changToNextGitHubToken(tokenIndex);
+                tokenIndex += 1;
+            }
+        }
+    } catch (err) {
+        console.log(err);
+        res.send(getErrorMsgCard(err.message, theme));
+    }
 };
