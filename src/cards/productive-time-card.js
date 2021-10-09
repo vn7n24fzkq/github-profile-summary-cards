b63bfc833e84fa2f9eb7ca21b7df4315d29b84eb
kerthcet <kerthcet@gmail.com>
2021-10-09 16:53:45 +0800

    feat: add timezone options to productive-time card (#59)
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- src/cards/productive-time-card.js
+++ src/cards/productive-time-card.js
@@ -3,30 +3,31 @@ const getProductiveTime = require('../github-api/productive-time');
 const productiveTimeCard = require('../templates/productive-time-card');
 const { writeSVG } = require('../utils/file-writer');
 
-const createProductiveTimeCard = async function (username) {
-    const productiveTimeData = await getProductiveTimeData(username);
+const createProductiveTimeCard = async function (username, timezone) {
+    const productiveTimeData = await getProductiveTimeData(username, timezone);
     for (const themeName of ThemeMap.keys()) {
-        const svgString = getProductiveTimeSVG(productiveTimeData, themeName);
+        const svgString = getProductiveTimeSVG(productiveTimeData, themeName, timezone);
         // output to folder, use 4- prefix for sort in preview
         writeSVG(themeName, '4-productive-time', svgString);
     }
 };
 
-const getProductiveTimeSVGWithThemeName = async function (username, themeName) {
+const getProductiveTimeSVGWithThemeName = async function (username, themeName, timezone) {
     if (!ThemeMap.has(themeName)) throw new Error('Theme does not exist');
-    const productiveTimeData = await getProductiveTimeData(username);
-    return getProductiveTimeSVG(productiveTimeData, themeName);
+    const productiveTimeData = await getProductiveTimeData(username, timezone);
+    return getProductiveTimeSVG(productiveTimeData, themeName, timezone);
 };
 
-const getProductiveTimeSVG = function (productiveTimeData, themeName) {
+const getProductiveTimeSVG = function (productiveTimeData, themeName, timezone) {
     const svgString = productiveTimeCard(
         productiveTimeData,
-        ThemeMap.get(themeName)
+        ThemeMap.get(themeName),
+        timezone
     );
     return svgString;
 };
 
-const getProductiveTimeData = async function (username) {
+const getProductiveTimeData = async function (username, timezone) {
     const until = new Date();
     const since = new Date();
     since.setFullYear(since.getFullYear() - 1);
@@ -43,10 +44,32 @@ const getProductiveTimeData = async function (username) {
         chartData[hour] += 1;
     }
 
-    return chartData;
+    return transferDataWithTimezone(timezone, chartData);
 };
+
+const transferDataWithTimezone = function (timezone, chartData) {
+    const offset = getHourOffset(timezone)
+    const newChartData = new Array(24);
+
+    for (let i = 0; i < chartData.length; i++) {
+        newChartData[(i+offset)%24] = chartData[i]
+    }
+
+    return newChartData
+}
+
+const getHourOffset = function (timezone) {
+    process.env.TZ = timezone || "Europe/London"
+    var lag = new Date().getTimezoneOffset()/60
+
+    if (lag > 0) {
+        return 24 - lag
+    }
+    return -lag
+}
 
 module.exports = {
     createProductiveTimeCard,
     getProductiveTimeSVGWithThemeName,
+    transferDataWithTimezone,
 };
