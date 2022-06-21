7a6484a6626b1874a4645da00bed55cab1d8e13d
beshoux <55193319+akai54@users.noreply.github.com>
2022-06-21 15:32:43 +0200

    feat(theme): add 2077 Theme (#93)

--- src/const/theme.ts
+++ src/const/theme.ts
@@ -29,5 +29,6 @@ ThemeMap.set('nord_dark', new Theme('#eceff4', '#e5e9f0', '#2e3440', '#eceff4', 
 ThemeMap.set('github', new Theme('#0366d6', '#586069', '#ffffff', '#e4e2e2', '#586069', '#40c463'));
 ThemeMap.set('github_dark', new Theme('#0366d6', '#77909c', '#0d1117', '#2e343b', '#8b949e', '#40c463'));
 ThemeMap.set('radical', new Theme('#fe428e', '#a9fef7', '#141321', '#141321', '#f8d847', '#ae81ff'));
+ThemeMap.set('2077', new Theme('#ff0055', '#03d8f3', '#141321', '#141321', '#FCEE0C', '#00ffc8'));
 ThemeMap.set('gruvbox', new Theme('#fabd2f', '#8ec07c', '#282828', '#282828', '#fe8019', '#fe8019'));
 ThemeMap.set('tokyonight', new Theme('#70a5fd', '#38bdae', '#1a1b27', '#1a1b27', '#bf91f3', '#bf91f3'));
